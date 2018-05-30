3473ea461bfd0e1dd9dc49d4827c02a8ddaeb0d5
Jason White <whitej8@gmail.com>
2018-05-30 17:03:15 -0400

    Pull Data; Display Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Flexbox from 'flexbox-react';
+
 import './App.css';
 
+var Api = require('./utils/api');
+
 class App extends Component {
+  state = {
+    daily: null
+  };
+
+  cities = ['Atlanta']; 
+  citiesWeather = []; // API cache
+  currentCity = 0; // Index of current city displayed
+
+  // Called before the render method is executed
+  componentWillMount = () => {
+    this.fetchData();
+  }
+  
+  fetchData = () => {
+    
+    // Get the data from the cache if possible
+    if (this.citiesWeather[this.currentCity]) {
+        this.updateData();   
+    } else {
+        // Request new data to the API
+        Api.get(this.cities[this.currentCity])
+        .then(function(data) {
+            this.citiesWeather[this.currentCity] = data;
+            this.updateData();
+        }.bind(this));
+    }
+  }
+  
+  updateData = () => {
+    // Update the data for the UI
+    this.setState({
+        daily: this.citiesWeather[this.currentCity].list,
+    });    
+  }
+
+
   render() {
+    if (!this.state.daily) { return <div/> } // do not render until data is present
+
     return (
-      <div className="App">
-        <div className="App-header">
-        </div>
-      </div>
+      <div className="App"> 
+        <AppHeader />
+      </div>  
     );
   }
 }
-
 export default App;
+
+class AppHeader extends Component {
+  render() {
+    return (
+      <Flexbox element="header" height="60px">
+          <logo />
+      </Flexbox>
+    );
+  }
+}
